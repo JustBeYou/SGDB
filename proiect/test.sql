b338f59d1c9e148a76227a471fe6c22536af3ba5
JustBeYou <mihailferaru2000@gmail.com>
2020-11-05 13:46:10 +0200

    project: cleanup

--- proiect/test.sql
+++ proiect/test.sql
@@ -3,11 +3,13 @@ select * from quotation;
 select * from account;
 select * from security;
 
-EXECUTE stock_exchange.ask(5, 'VU', 6, 1);
+EXECUTE stock_exchange.ask(6, 'VU', 1, 1);
 EXECUTE stock_exchange.bid(5, 'VU', 100, 10);
-EXECUTE stock_exchange.cancel_bid(31);
+EXECUTE stock_exchange.cancel_ask(1);
 SELECT o.amount
         FROM own o
         WHERE o.account_id = 6 AND 
             o.ticker = 'VU' AND
             o.amount >= 1;
+
+EXECUTE dbms_output.put_line(stock_exchange.quot_fees('FUND_SECURITY'));
