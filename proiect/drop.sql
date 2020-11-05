345ba3b80b82bada9f5c284c32dc965b2bf2455c
JustBeYou <mihailferaru2000@gmail.com>
2020-11-05 13:45:53 +0200

    project: sample data and dropping

--- proiect/drop.sql
+++ proiect/drop.sql
@@ -1,15 +1,25 @@
+/* Just drop everything we ever made */
+
 drop sequence account_ids;
 drop sequence quotation_ids;
+drop sequence trade_ids;
 
 drop trigger account_auto_id;
 drop trigger quotation_auto_id;
+drop trigger new_security_type;
+drop trigger trade_auto_id;
 
 drop table own;
 drop table trade;
 drop table quotation;
+drop table security_type_info;
 drop table stock_security;
 drop table fund_security;
 drop table bond_security;
 drop table security;
 drop table account;
 drop table holder;
+
+drop package se_core;
+drop package se_utils;
+drop package se_types;
