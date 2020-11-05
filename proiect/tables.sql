b6d93d0a7ca56d6671bcfaf2127836861594aa04
JustBeYou <mihailferaru2000@gmail.com>
2020-11-05 13:44:14 +0200

    project: updated SE tables

--- proiect/tables.sql
+++ proiect/tables.sql
@@ -1,64 +1,43 @@
+/**
+ * Stock Exchange Tables
+ *
+ * This file contains all table definitions and their mandatory
+ * triggers like auto-incrementing and default imformation generating.
+ *
+ * Note: As SQL is no aware of custom type we will annotate each
+ * column with a comment to specify the PL/SQL scalar type
+ */
+
 --- Security
 create table security (
-    ticker varchar2(5) not null,
-    name varchar2(256) not null,
-    total_shares number(15) default 0 not null check (total_shares > 0),
-    last_price number(15) default 0 not null,
+    ticker       /*abbreviation*/    varchar2(10) not null,
+    name         /*long_thing_name*/ varchar2(256) not null,
+    total_shares /*quantity*/        number(15) default 0 not null check (total_shares > 0),
+    last_price   /*quantity*/        number(15) default 0 not null,
+    type         /*type_string*/     varchar2(16) not null,
     
     constraint security_pk primary key (ticker)
-);
-
-create table stock_security (
-    ticker varchar2(5) not null,
-    constraint stock_security_pk primary key (ticker),
-    constraint stock_security_fk
-        foreign key (ticker)
-        references security(ticker),
-    
-    type varchar2(16) not null check (type in ('COMMON', 'PREFERRED')),
-    pays_dividents char default 0 not null check (pays_dividents in (0, 1))
-);
-
-create table fund_security (
-    ticker varchar2(5) not null,
-    constraint fund_security_pk primary key (ticker),
-    constraint fund_security_fk
-        foreign key (ticker)
-        references security(ticker),
-    
-    type varchar2(16) not null check (type in ('HEDGE', 'MUTUAL', 'BOND'))
-);
-
-create table bond_security (
-    ticker varchar2(5) not null,
-    constraint bond_security_pk primary key (ticker),
-    constraint bond_security_fk
-        foreign key (ticker)
-        references security(ticker),
-    
-    interest_rate number(2, 4) not null check (interest_rate >= 0.01)
 );
 
 --- Holder
 create table holder (
     -- social security number/registration number
-    legal_id varchar2(64) not null,
-    
-    legal_name varchar2(256) not null,
-    legal_status varchar(16) not null check (legal_status in ('PERSON', 'COMPANY')),
-    physical_address varchar2(512) not null,
-    billing_address varchar2(512) not null,
-    email varchar2(64) unique not null,
-    phone varchar2(20) unique not null,
+    legal_id         /*thing_name*/      varchar2(64) not null,
+    legal_name       /*long_thing_name*/ varchar2(256) not null,
+    legal_status     /*type_string*/     varchar2(16) not null check (legal_status in ('PERSON', 'COMPANY')),
+    physical_address /*long_thing_name*/ varchar2(256) not null,
+    billing_address  /*long_thing_name*/ varchar2(256) not null,
+    email            /*thing_name*/      varchar2(64) unique not null,
+    phone            /*thing_name*/      varchar2(64) unique not null,
     
     constraint holder_pk primary key (legal_id)
 );
 
 --- Account
 create table account (
-    id integer not null,
-    holder_id varchar2(64) not null,
-    capital number(15) default 0 not null,
+    id        /*id*/         integer not null,
+    holder_id /*thing_name*/ varchar2(64) not null,
+    capital  /*quantity*/    number(15) default 0 not null,
     
     constraint account_pk primary key (id),
     constraint holder_fk 
@@ -79,9 +58,9 @@ end;
 
 --- account Owns security
 create table own (
-    account_id integer not null,
-    ticker varchar2(5) not null,
-    amount number(15) not null,
+    account_id /*id*/           integer not null,
+    ticker     /*abbreviation*/ varchar2(10) not null,
+    amount     /*quantity*/     number(15) not null,
     
     constraint own_pk primary key (account_id, ticker),
     
@@ -95,17 +74,17 @@ create table own (
 
 --- Quotation
 create table quotation (
-    id integer not null,
+    id /*id*/ integer not null,
 
-    type varchar2(3) not null check (type in ('ASK', 'BID')),
-    fulfilled char default 0 not null check (fulfilled in (0, 1)),
-    deleted char default 0 not null check (deleted in (0, 1)),
+    type      /*type_string*/ varchar2(16) not null check (type in ('ASK', 'BID')),
+    fulfilled /*status*/      char default 0 not null check (fulfilled in (0, 1)),
+    deleted   /*status*/      char default 0 not null check (deleted in (0, 1)),
 
-    ticker varchar2(5) not null,
-    account_id integer not null,
+    ticker     /*abbreviation*/ varchar2(10) not null,
+    account_id /*id*/           integer not null,
     
-    amount number(15) not null,
-    price number(15) not null,
+    amount /*quantity*/ number(15) not null,
+    price  /*quantity*/ number(15) not null,
     
     constraint quotation_pk primary key (id),
     
@@ -130,19 +109,20 @@ end;
 
 --- Trade
 create table trade (
-    ask_id integer not null,
-    bid_id integer not null,
+    id     /*id*/ integer not null,
+    ask_id /*id*/ integer not null,
+    bid_id /*id*/ integer not null,
 
-    asker_account_id integer not null,
-    bidder_account_id integer not null,
-    market_maker_account_id integer not null,
+    asker_account_id        /*id*/ integer not null,
+    bidder_account_id       /*id*/ integer not null,
+    market_maker_account_id /*id*/ integer not null,
     time timestamp with time zone,
     
-    ticker varchar2(5) not null,
-    amount number(15) not null,
-    price number(15) not null,
+    ticker /*abbreviation*/ varchar2(10) not null,
+    amount /*quantity*/     number(15) not null,
+    price  /*quantity*/     number(15) not null,
     
-    constraint trade_pk primary key (ask_id, bid_id),
+    constraint trade_pk primary key (id, ask_id, bid_id),
     
     constraint ask_fk
         foreign key (ask_id)
@@ -164,4 +144,74 @@ create table trade (
     constraint traded_asset_fk
         foreign key (ticker)
         references security(ticker)
+);
+
+create sequence trade_ids start with 1;
+create or replace trigger trade_auto_id 
+before insert on trade
+for each row
+begin
+  select trade_ids.nextval
+  into   :new.id
+  from   dual;
+end;
+/
+
+--- Security Types
+
+create table security_type_info (
+    name /*thing_name*/ varchar2(64) not null,
+    fee  /*percentage*/ number(5, 3) default 0.02 not null,
+    
+    constraint security_type_info_pk primary key (name)
+);
+
+CREATE TRIGGER new_security_type AFTER CREATE
+    ON SCHEMA
+    DECLARE
+    BEGIN
+        FOR ctable IN (
+            SELECT a.table_name
+            FROM all_tables a
+            WHERE a.table_name LIKE '%_SECURITY'
+             AND a.table_name NOT IN (
+                    SELECT b.name as table_name from
+                    security_type_info b
+                )
+        ) LOOP
+            INSERT INTO security_type_info (name)
+            VALUES (ctable.table_name);
+        END LOOP;
+    END;
+/
+
+create table stock_security (
+    ticker /*abbreviation*/ varchar2(10) not null,
+    constraint stock_security_pk primary key (ticker),
+    constraint stock_security_fk
+        foreign key (ticker)
+        references security(ticker),
+    
+    type           /*type_string*/ varchar2(16) not null check (type in ('COMMON', 'PREFERRED')),
+    pays_dividents /*status*/      char default 0 not null check (pays_dividents in (0, 1))
+);
+
+create table fund_security (
+    ticker /*abbreviation*/ varchar2(10) not null,
+    constraint fund_security_pk primary key (ticker),
+    constraint fund_security_fk
+        foreign key (ticker)
+        references security(ticker),
+    
+    type /*type_string*/ varchar2(16) not null check (type in ('HEDGE', 'MUTUAL', 'BOND'))
+);
+
+create table bond_security (
+    ticker /*abbreviation*/ varchar2(10) not null,
+    constraint bond_security_pk primary key (ticker),
+    constraint bond_security_fk
+        foreign key (ticker)
+        references security(ticker),
+    
+    interest_rate /*percentage*/ number(5, 3) not null check (interest_rate >= 0.01)
 );
