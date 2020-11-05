13ce6998c68f1812ed1c07d7d3c96bccfa028d5a
JustBeYou <mihailferaru2000@gmail.com>
2020-11-05 13:44:25 +0200

    project: generic types

---
+++ proiect/se_types.sql
@@ -0,0 +1,19 @@
+/**
+ * Stock Exchange Types Package
+ * 
+ * This file contains all generic types used for our 
+ * computations. They are used to reduce the confusion and 
+ * redundancy of using base types like varchar2(XXX) and number(X, Y)
+ */
+ 
+CREATE OR REPLACE PACKAGE se_types AS
+    SUBTYPE id IS integer;
+    SUBTYPE quantity IS number(15);       -- for prices/shares
+    SUBTYPE type_string IS varchar2(16);  -- for statuses and row types
+    SUBTYPE abbreviation IS varchar2(10); -- for abbreviated names like tickers
+    SUBTYPE thing_name IS varchar2(64);  -- common names like legal name
+    SUBTYPE long_thing_name IS varchar2(256); -- long names like companies
+    SUBTYPE status IS char; -- for statuses, from 0 to 255
+    SUBTYPE percentage IS number(5, 3); -- for numbers expressing percentages (ex. 0.1 = 10%)
+END se_types;
+/
